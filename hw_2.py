5e95b4b36a366eec2c8039b631e255599cdef7a3
Eldar Adzhiev <e.adzhiev93@gmail.com>
2021-10-06 21:05:15 +0300

    4) Реализовать 15 варианта сравнения int переменных с операторами >, <, >=, <=, !=. Pезультаты весвести в консоль.

--- hw_2.py
+++ hw_2.py
@@ -13,3 +13,80 @@ new_float = 64.4
 new2_float = 77.13
 new3_float =732.6
 
+# 4) Реализовать 15 варианта сравнения int переменных с операторами >, <, >=, <=, !=. Pезультаты весвести в консоль.
+if new_int > new2_int:
+    print('new_int')
+else:
+    print('new2_int')
+
+if new_int < new3_int:
+    print('new_int')
+else:
+    print('new3_int')
+
+if new_int >= new4_int:
+    print('new_int')
+else:
+    print('False')
+
+if new_int <= new4_int:
+    print('True')
+else:
+    print('False')
+
+if new_int != new4_int:
+    print('True')
+else:
+    print('False')
+
+if new2_int > new3_int:
+    print('new2_int')
+else:
+    print('new2_int')
+
+if new2_int < new3_int:
+    print('new2_int')
+else:
+    print('new3_int')
+
+if new2_int >= new4_int:
+    print('new2_int')
+else:
+    print('new4_int')
+
+if new2_int <= new4_int:
+    print('new2_int')
+else:
+    print('new4_int')
+
+if new2_int != new4_int:
+    print('new2_int')
+else:
+    print('new4_int')
+
+if new3_int >= new4_int:
+    print('new3_int')
+else:
+    print('new4_int')
+
+if new3_int <= new4_int:
+    print('new3_int')
+else:
+    print('new4_int')
+
+if new3_int != new4_int:
+    print('new3_int')
+else:
+    print('new4_int')
+
+if new3_int > new4_int:
+    print('new3_int')
+else:
+    print('new4_int')
+
+if new3_int < new4_int:
+    print('new3_int')
+else:
+    print('new4_int')
+
+
