32ee0e317f31396c79631bdfede58165d629c9db
Eldar Adzhiev <e.adzhiev93@gmail.com>
2021-11-08 21:20:31 +0300

    home work 4, Cycles while and for

---
+++ hw_4/hw_4.py
@@ -0,0 +1,65 @@
+count = 0
+range_count = 10
+for_count = 0
+run = True
+
+while run:
+    print("Hello Cycle")
+
+while run:
+    print("Step =", count)
+    count += 1
+
+while count < range_count:
+    print("Step =", count)
+    count += 1
+
+while count<range_count:
+    print("Step =", count)
+    count += 1
+    if count == 3:
+        print("Step =", count, 'if body')
+
+while run:
+    print("Step =", count)
+    count += 1
+    if count == range_count:
+        break
+        print("STOP", count)
+
+
+for item in range (for_count, range_count):
+    print("Step =", item)
+
+
+for item in range (30):
+    print("Step =", item)
+    if item == 5:
+        print("item =", item)
+    if item == 10:
+        print("item =", item)
+    if item < 4:
+        print("Item <", item)
+    if item >= 27:
+        print("Item >=", item)
+
+
+for item in range (range_count + 1):
+    print("Step =", item)
+    if item == 7:
+        inner_count = 0
+        print("--inner_count =", inner_count)
+        for inner_item in range(item):
+            print("------Inner_step =", inner_item)
+            if inner_item == 5:
+                inner_count = inner_item
+        print("-- inner_count =", inner_count)
+
+
+
+for item in range(20):
+    print("Step =", item)
+    if 7< item < 12:
+        print("if_item =", item)
+        continue
+print("End_iteration =", item)
