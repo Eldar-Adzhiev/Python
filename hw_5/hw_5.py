d8cb20884d610e5ce60e1ce768d10859268e0070
Eldar Adzhiev <e.adzhiev93@gmail.com>
2021-11-15 21:07:03 +0300

    hw 5 task 13

--- hw_5/hw_5.py
+++ hw_5/hw_5.py
@@ -55,17 +55,52 @@ list_1 = list(range(71))
 # chunkedList(list_random,5)
 
 # Написать функцию которая, получив на вход список целых чисел, вернёт 2 списка, список чётных и список нечётных чисел.
-def output_of_even_and_odd_numbers(list_name):
-    list_of_odd_numbers = []
-    for i in list_name:
-        if i % 2==1:
-            list_of_odd_numbers.append(i)
-    print(list_of_odd_numbers)
-    list_of_even_numbers = []
-    for k in list_name:
-        if k % 2==0:
-            list_of_even_numbers.append(k)
-    print(list_of_even_numbers)
+# def output_of_even_and_odd_numbers(list_name):
+#     list_of_odd_numbers = []
+#     for i in list_name:
+#         if i % 2==1:
+#             list_of_odd_numbers.append(i)
+#     print(list_of_odd_numbers)
+#     list_of_even_numbers = []
+#     for k in list_name:
+#         if k % 2==0:
+#             list_of_even_numbers.append(k)
+#     print(list_of_even_numbers)
+#
+#
+# output_of_even_and_odd_numbers(list_1)
+
+# Написать скрипт который сгенерирует список под названием 5_stars из списков по 5 элементов целых чисел.
+# list_for_stars_1 = [1,3,34,5,2]
+# list_for_stars_2 = [91,7,62,4,87]
+# stars_5 = []
+# stars_5.append(list_for_stars_1)
+# stars_5.append(list_for_stars_2)
+# print(stars_5)
+
+# Написать скрипт который выведет список из сумм каждого внутреннего списка из  5_stars
+
+# result_sum_lists_of_list = []
+# for list_number in stars_5:
+#     result_sum_lists_of_list.append(sum(list_number))
+#
+# print(result_sum_lists_of_list)
 
 
-output_of_even_and_odd_numbers(list_1)
+# 13) Написать функцию которая на вход получает список 5_stars, а вернёт 2 списка.
+# В одном списке внутренние списки из 5_stars сумма чисел которых >= 100,
+# а другой сумма чисел которых < 100. Если какого-то списка не получится, то вместо него вернуть текст “No lists”
+
+stars_5 = [[5,12,45,2,4],[1,23,4,66,7], [1,2,3,4,5]]
+
+def star_5(list_name):
+    list1 = []
+    list2 = []
+
+
+
+
+
+star_5(stars_5)
+
+
