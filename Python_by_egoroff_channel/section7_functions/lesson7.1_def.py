4b6c896a9a2e911d95b0313536c293da44aa3203
Eldar Adzhiev <e.adzhiev93@gmail.com>
2022-05-12 09:36:32 +0300

    7.1 Определение и вызов функции. Инструкция def

---
+++ Python_by_egoroff_channel/section7_functions/lesson7.1_def.py
@@ -0,0 +1,46 @@
+#  7.1 Определение и вызов функции. Инструкция def
+
+# Определение функции
+
+def sayHello():
+    print("hello")
+    print("world")
+
+# Вызов функции
+sayHello()
+
+# Функция с позиционным аргументом
+
+def square(x):
+    print("x =", x**2)
+
+square(10)
+
+#=====================================================================
+
+def multiplay(a,b):
+    print(a*b)
+
+multiplay(4,2)
+
+
+if 5>1:
+    def primer():
+        print("HELLO")
+else:
+    def primer():
+        print("hello")
+
+primer()
+
+
+
+
+
+
+
+
+
+
+
+
