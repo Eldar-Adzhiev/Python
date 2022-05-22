fd2c875d7c4eb1b39b455955012bc0ee0e5d564b
Eldar Adzhiev <e.adzhiev93@gmail.com>
2022-05-22 06:45:38 +0300

    8.2 Импорт стандартных модулей

---
+++ Python_by_egoroff_channel/section8_modules/lesson8.2_import_standart_moduls.py
@@ -0,0 +1,51 @@
+# 8.2 Импорт стандартных модулей
+
+import pprint
+# import math as m
+# from math import e, pi, factorial
+# from math import *  # импорт все функции модуля маз
+
+def say_hello(name):
+    print(f'Hello,{name}')
+
+
+def summa(*args):
+    return sum(args)
+
+
+def factorial(n):
+    pr = 1
+    for i in range(1, n+1):
+        pr += 1
+    return pr
+
+my_str = "YOU'RE BREATHTAKING"
+my_num1 =2
+my_num2 = 3
+
+# pprint.pprint(locals())
+# pprint.pprint(dir(m))
+pprint.pprint(pi)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
