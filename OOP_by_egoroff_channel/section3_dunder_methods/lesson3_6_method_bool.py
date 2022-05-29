5e2c588b06932072629d8e01189c459f3550baa4
Eldar Adzhiev <e.adzhiev93@gmail.com>
2022-05-29 19:39:30 +0300

    3.6 Магический метод __bool__

---
+++ OOP_by_egoroff_channel/section3_dunder_methods/lesson3_6_method_bool.py
@@ -0,0 +1,17 @@
+# 3.6 Магический метод __bool__
+
+class Point:
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __len__(self):
+        print('call __len__')
+        return abs(self.x - self.y)
+
+    def __bool__(self):
+        print('call __bool__')
+        return self.x !=0 or self.y != 0
+
+
